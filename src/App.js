5defd5e1344193a146ade307457d33277b62f6a1
Chelsea Patterson <chelseapat77@gmail.com>
2024-02-18 18:19:59 +1300

    feat: image slider

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Accordion from './components/accordion';
 import RandomColour from './components/random-color';
 import React from 'react';
 import StarRating from './components/star-rating';
+import ImageSlider from './components/image-slider';
 
 function App () {
     return (
@@ -15,7 +16,14 @@ function App () {
             {/* <RandomColour /> */}
 
             {/* Star rating component */}
-            <StarRating noOfStars={10} />
+            {/* <StarRating noOfStars={10} /> */}
+
+            {/* Image slider component */}
+            <ImageSlider
+                url={"https://picsum.photos/v2/list"}
+                page={"1"}
+                limit={"10"}
+            />
         </div>
     );
 }

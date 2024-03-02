6d89ded72ee39e26c7305aa6fc24bf1e2fdec545
Chelsea Patterson <chelseapat77@gmail.com>
2024-03-03 11:03:09 +1300

    feat: add a load more data button for a list of products

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import RandomColour from './components/random-color';
 import React from 'react';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
+import LoadMoreData from 'components/load-more-data';
 
 function App () {
     return (
@@ -19,11 +20,14 @@ function App () {
             {/* <StarRating noOfStars={10} /> */}
 
             {/* Image slider component */}
-            <ImageSlider
+            {/* <ImageSlider
                 url={"https://picsum.photos/v2/list"}
                 page={"1"}
                 limit={"10"}
-            />
+            /> */}
+
+            {/* Load more products component */}
+            <LoadMoreData />
         </div>
     );
 }
